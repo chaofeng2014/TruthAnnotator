18469f443acda275c064b028b44c7c9f966a8ba3
Fang Wang <fangwang@Yus-MacBook-Pro.local>
2014-09-13 13:29:05 -0500

    popup page enhanced

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -2,28 +2,24 @@ Parse.initialize("Jbz8IatuSOpr7xmnNXBpnCcN1cj2ox9sPzsqggak", "anMcouVSWbzeHoJmFJ
 
 $(document).ready(function(){
     var currentUserId = showNickname();
-    queryStat(currentUserId);
-    $("#logout").click(function(){
-        Parse.User.logOut();
-        //sendToContentLogout();
-        removeStorage();
-        chrome.browserAction.setIcon({path:'../../img/T-400_white.png'}, function()
-        {
-          window.close();
-        });
+    queryStat(currentUserId, function(result){
+      console.log(result);
+      bindEvent();
     });
 });
 
-function queryStat(currentUserId) {
-  var btnup = '<span class="pop-btn" id=thumbup><i class="fa fa-thumbs-up"></i></span>';
-  var btndown = '<span class="pop-btn" id=thumbdown><i class="fa fa-thumbs-down"></i></span>';
+function queryStat(currentUserId, _callback) {
+  var btnup_pop = '<span class="btnup" id=thumbup_pop><i class="fa fa-thumbs-up"></i></span>';
+  var btndown_pop = '<span class="btndown" id=thumbdown_pop><i class="fa fa-thumbs-down"></i></span>';
+  var btnup_con = '<span class="btnup" id=thumbup_con><i class="fa fa-thumbs-up"></i></span>';
+  var btndown_con = '<span class="btndown" id=thumbdown_con><i class="fa fa-thumbs-down"></i></span>';
   var Annotation = Parse.Object.extend("Annotation");
   var query = new Parse.Query(Annotation);
   //FIXME may need better query method, now cost 
   query.descending("numberOfAgree");
   query.first({
     success: function(object) {
-      queryCurrentUserPop(object.id, currentUserId, function(result){
+      queryCurrentUser(object.id, currentUserId, function(result){
           //console.log(result[0]);
           var opinion;
           if (result.length === 0){opinion = 0;}
@@ -32,30 +28,47 @@ function queryStat(currentUserId) {
           var author = object.get('userName');
           var agree = object.get('numberOfAgree');
           var disagree = object.get('numberOfDisagree');
-          var inHtml_title = '<p class=stat-mostAgree>Most Popular <br></p>'; 
-          var inHtml_text = '<p class=stat-text>'+'"' + selectedText + '"</p>';
+          var source = object.get('hostDomain');
+          var inHtml_title = '<p class=stat-mostAgree>Most popular (' + source + ')<br></p>'; 
+          var inHtml_text = '<p class=stat-text>'+'" ' + selectedText + ' "</p>';
           var inHtml_author = '<p class=stat-author>' +'--by '+ author + '</p>';
           var inHtml_agree = '<span class=stat-agree id=pop_agree>' + agree + '</span>';
           var inHtml_disagree = '<span class=stat-agree id=pop_disagree>' + disagree + '</span>';
-          var inHtml_pop = inHtml_title + inHtml_text + inHtml_author + btnup +inHtml_agree +  btndown + inHtml_disagree;
+          if(opinion === 1){
+            btnup_pop = '<span class="btnup" id=thumbup_pop style="color: blue;"><i class="fa fa-thumbs-up"></i></span>';
+          }
+          else if(opinion === -1){
+            btndown_pop = '<span class="btndown" id=thumbdown_pop style="color: blue;"><i class="fa fa-thumbs-down"></i></span>';
+          }
+          var inHtml_pop = inHtml_title + inHtml_text + inHtml_author + btnup_pop +inHtml_agree +  btndown_pop + inHtml_disagree;
           query.descending("numberOfDisagree");
           query.first({
             success: function(object) {
-              queryCurrentUserPop(object.id, currentUserId, function(result){
-                //console.log(object);
+              queryCurrentUser(object.id, currentUserId, function(result){
+                var opinion;
+                if (result.length === 0){opinion = 0;}
+                else {opinion = result[0].get('opinion');}
                 var selectedText = object.get('selectedText');
                 var author = object.get('userName');
                 var agree = object.get('numberOfAgree');
                 var disagree = object.get('numberOfDisagree');
-                var inHtml_title = '<p class=stat-mostDisagree>Most Controversial <br></p>'; 
-                var inHtml_text = '<p class=stat-text>'+'"' + selectedText + '"</p>';
+                var source = object.get('hostDomain');
+                var inHtml_title = '<p class=stat-mostDisagree>Most controversial (' + source + ')<br></p>'; 
+                var inHtml_text = '<p class=stat-text>'+'" ' + selectedText + ' "</p>';
                 var inHtml_author = '<p class=stat-author>' +'--by '+ author + '</p>';
                 var inHtml_agree = '<span class=stat-agree id=con_agree>' + agree + '</span>';
                 var inHtml_disagree = '<span class=stat-agree id=con_disagree>' + disagree + '</span>';
-                var inHtml_con = inHtml_title + inHtml_text + inHtml_author + btnup +inHtml_agree +  btndown + inHtml_disagree;
-                inHtml= inHtml_pop + inHtml_con;
+                if(opinion === 1){
+                  btnup_con = '<span class="btnup" id=thumbup_con style="color: blue;"><i class="fa fa-thumbs-up"></i></span>';
+                }
+                else if(opinion === -1){
+                  btndown_con = '<span class="btndown" id=thumbdown_con style="color: blue;"><i class="fa fa-thumbs-down"></i></span>';
+                }
+                var inHtml_con = inHtml_title + inHtml_text + inHtml_author + btnup_con +inHtml_agree +  btndown_con + inHtml_disagree;
+                inHtml= '<hr>' + inHtml_pop + '<hr>'+ inHtml_con;
                 $("#tweet-stat").html(inHtml);
-                bindEvent();
+                var result = 'testt';
+                _callback(result);
               });
             },
             error: function(error) {
@@ -68,25 +81,9 @@ function queryStat(currentUserId) {
       alert("Error: " + error.code + " " + error.message);
     }
   });
-
 }
 
-function queryCurrentUserPop(annotationId, userId, _callback){
-  var UserAnnotation = Parse.Object.extend("UserAnnotation");
-  var query = new Parse.Query(UserAnnotation);
-  query.equalTo('annotationId', annotationId);
-  query.equalTo('userId', userId);
-  query.find({
-      success: function(results) {
-        _callback(results);
-      },
-      error: function(error) {
-        alert("Error: " + error.code + " " + error.message);
-      }
-  });
-}
-
-function queryCurrentUserCon(annotationId, userId, _callback){
+function queryCurrentUser(annotationId, userId, _callback){
   var UserAnnotation = Parse.Object.extend("UserAnnotation");
   var query = new Parse.Query(UserAnnotation);
   query.equalTo('annotationId', annotationId);
@@ -101,14 +98,31 @@ function queryCurrentUserCon(annotationId, userId, _callback){
   });
 }
   
-
 function bindEvent(){
-  $("#thumbup").click(function(){
-    console.log('clicked up');
+  $("#thumbup_pop").click(function(){
+    console.log('popclicked up');
   });
   
-  $("#thumbdown").click(function(){
-    console.log('clicked down');
+  $("#thumbdown_pop").click(function(){
+    console.log('popclicked down');
+  });
+
+  $("#thumbup_con").click(function(){
+    console.log('conclicked up');
+  });
+  
+  $("#thumbdown_con").click(function(){
+    console.log('conclicked down');
+  });
+ 
+  $("#logout").click(function(){
+      Parse.User.logOut();
+      //sendToContentLogout();
+      removeStorage();
+      chrome.browserAction.setIcon({path:'../../img/T-400_white.png'}, function()
+      {
+        window.close();
+      });
   });
 }
 
