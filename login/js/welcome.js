6e0f2f58c78a1f4f45c2bd16a104dc18399b88aa
chijuzipi <yuzhoubrother@gmail.com>
2014-09-13 16:18:34 -0500

    popup page with votes

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -1,5 +1,6 @@
 Parse.initialize("Jbz8IatuSOpr7xmnNXBpnCcN1cj2ox9sPzsqggak", "anMcouVSWbzeHoJmFJBcJYrmg8XtzUatOt7hrgJX");
 
+var _userOpinion;
 $(document).ready(function(){
     var currentUserId = showNickname();
     queryStat(currentUserId, function(result){
@@ -9,13 +10,12 @@ $(document).ready(function(){
 });
 
 function queryStat(currentUserId, _callback) {
-  var btnup_pop = '<span class="btnup" id=thumbup_pop><i class="fa fa-thumbs-up"></i></span>';
-  var btndown_pop = '<span class="btndown" id=thumbdown_pop><i class="fa fa-thumbs-down"></i></span>';
-  var btnup_con = '<span class="btnup" id=thumbup_con><i class="fa fa-thumbs-up"></i></span>';
-  var btndown_con = '<span class="btndown" id=thumbdown_con><i class="fa fa-thumbs-down"></i></span>';
+  var btnup_pop = '<span class="btnup" id=thumbup_pop style="color:gray;"><i class="fa fa-thumbs-up"></i></span>';
+  var btndown_pop = '<span class="btndown" id=thumbdown_pop style="color:gray;"><i class="fa fa-thumbs-down"></i></span>';
+  var btnup_con = '<span class="btnup" id=thumbup_con style="color:gray;"><i class="fa fa-thumbs-up"></i></span>';
+  var btndown_con = '<span class="btndown" id=thumbdown_con style="color:gray;"><i class="fa fa-thumbs-down"></i></span>';
   var Annotation = Parse.Object.extend("Annotation");
   var query = new Parse.Query(Annotation);
-  //FIXME may need better query method, now cost 
   query.descending("numberOfAgree");
   query.first({
     success: function(object) {
@@ -100,22 +100,22 @@ function queryCurrentUser(annotationId, userId, _callback){
   
 function bindEvent(){
   $("#thumbup_pop").click(function(){
-    console.log('popclicked up');
+    processClick($(this));
   });
   
-  $("#thumbdown_pop").click(function(){
-    console.log('popclicked down');
+  $("#thumbdown_pop").click(function(event){
+    processClick($(this));
   });
 
-  $("#thumbup_con").click(function(){
-    console.log('conclicked up');
+  $("#thumbup_con").click(function(event){
+    processClick($(this));
   });
   
-  $("#thumbdown_con").click(function(){
-    console.log('conclicked down');
+  $("#thumbdown_con").click(function(event){
+    processClick($(this));
   });
  
-  $("#logout").click(function(){
+  $("#welcome-logout").click(function(){
       Parse.User.logOut();
       //sendToContentLogout();
       removeStorage();
@@ -124,7 +124,76 @@ function bindEvent(){
         window.close();
       });
   });
+  
+  $("#welcome-close").click(function(){
+    window.close();
+  });
+
+  $(window).unload(function(){
+    updateParse();   
+  });
 }
+
+function processClick(node){
+  var num;
+  var numNode;
+
+  var counterBtn; 
+  var counterNumNode;
+  var counterNum;
+  if (node.attr('id') === 'thumbup_pop'){
+    numNode = $("#pop_agree");
+    num = parseInt(numNode.html());
+    counterBtn = $('#thumbdown_pop');
+    counterNumNode = $('#pop_disagree');
+    counterNum = parseInt(counterNumNode.html());
+  }
+
+  else if (node.attr('id') === 'thumbdown_pop'){
+    numNode = $("#pop_disagree");
+    num = parseInt(numNode.html());
+    counterBtn = $('#thumbup_pop');
+    counterNumNode = $('#pop_agree');
+    counterNum = parseInt(counterNumNode.html());
+  }
+  
+  else if (node.attr('id') === 'thumbup_con'){
+    numNode = $("#con_agree");
+    num = parseInt(numNode.html());
+    counterBtn = $('#thumbdown_con');
+    counterNumNode = $('#con_disagree');
+    counterNum = parseInt(counterNumNode.html());
+  }
+  
+  else {
+    numNode = $("#con_disagree");
+    num = parseInt(numNode.html());
+    counterBtn = $('#thumbup_con');
+    counterNumNode = $('#con_agree');
+    counterNum = parseInt(counterNumNode.html());
+  }
+
+  if(node.css("color") === "rgb(0, 0, 255)") {
+    node.css({"color" : "gray"});
+    num--;
+    numNode.html(num);
+    //counterBtn.css({"color":""});
+  }
+  else {
+    if(counterBtn.css("color") === "rgb(0, 0, 255)") {
+      counterNum--;
+      counterNumNode.html(counterNum);
+    }
+      node.css({"color" : "blue"});
+      num++;
+      console.log(counterBtn);
+      counterBtn.css({"color":"gray"});
+      numNode.html(num);
+  }
+}
+
+  
+  
 
 function removeStorage(){
   chrome.storage.sync.set({objectId: "", username: "", nickname:""}, function(){
