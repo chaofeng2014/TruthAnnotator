b073db9f372d775e5b3fff7b36f7582bfaa6945e
chijuzipi <yuzhoubrother@gmail.com>
2014-09-12 22:33:23 -0500

    popup page show statistics

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -1,55 +1,114 @@
 Parse.initialize("Jbz8IatuSOpr7xmnNXBpnCcN1cj2ox9sPzsqggak", "anMcouVSWbzeHoJmFJBcJYrmg8XtzUatOt7hrgJX");
 
 $(document).ready(function(){
-    showNickname();
-    queryStat();
+    var currentUserId = showNickname();
+    queryStat(currentUserId);
     $("#logout").click(function(){
         Parse.User.logOut();
         //sendToContentLogout();
         removeStorage();
         chrome.browserAction.setIcon({path:'../../img/T-400_white.png'}, function()
         {
-          console.log('iconCC');
           window.close();
         });
     });
 });
 
-function queryStat() {
+function queryStat(currentUserId) {
+  var btnup = '<span class="pop-btn" id=thumbup><i class="fa fa-thumbs-up"></i></span>';
+  var btndown = '<span class="pop-btn" id=thumbdown><i class="fa fa-thumbs-down"></i></span>';
   var Annotation = Parse.Object.extend("Annotation");
   var query = new Parse.Query(Annotation);
+  //FIXME may need better query method, now cost 
   query.descending("numberOfAgree");
   query.first({
     success: function(object) {
-      //console.log(object);
-      var selectedText = object.get('selectedText');
-      var author = object.get('userName');
-      var agree = object.get('numberOfAgree');
-      var inHtml1 = '<p class=stat-mostAgree>Most Agree <br></p>'; 
-      var inHtml2 = '<p class=stat-text>'+'"' + selectedText + '"</p>';
-      var inHtml3 = inHtml1 + inHtml2;
-      query.descending("numberOfDisagree");
-      query.first({
-        success: function(object) {
-          //console.log(object);
+      queryCurrentUserPop(object.id, currentUserId, function(result){
+          //console.log(result[0]);
+          var opinion;
+          if (result.length === 0){opinion = 0;}
+          else {opinion = result[0].get('opinion');}
           var selectedText = object.get('selectedText');
           var author = object.get('userName');
-          var agree = object.get('numberOfDisagree');
-          var inHtml4 = '<p class=stat-mostDisagree>Most disagree <br></p>'; 
-          var inHtml5 = '<p class=stat-text>'+'"' + selectedText + '"</p>';
-          var inHtml6 = inHtml4 + inHtml5;
-          inHtml= inHtml3 + inHtml6;
-          $("#tweet-stat").html(inHtml);
-
-        },
-        error: function(error) {
-          alert("Error: " + error.code + " " + error.message);
-        }
+          var agree = object.get('numberOfAgree');
+          var disagree = object.get('numberOfDisagree');
+          var inHtml_title = '<p class=stat-mostAgree>Most Popular <br></p>'; 
+          var inHtml_text = '<p class=stat-text>'+'"' + selectedText + '"</p>';
+          var inHtml_author = '<p class=stat-author>' +'--by '+ author + '</p>';
+          var inHtml_agree = '<span class=stat-agree id=pop_agree>' + agree + '</span>';
+          var inHtml_disagree = '<span class=stat-agree id=pop_disagree>' + disagree + '</span>';
+          var inHtml_pop = inHtml_title + inHtml_text + inHtml_author + btnup +inHtml_agree +  btndown + inHtml_disagree;
+          query.descending("numberOfDisagree");
+          query.first({
+            success: function(object) {
+              queryCurrentUserPop(object.id, currentUserId, function(result){
+                //console.log(object);
+                var selectedText = object.get('selectedText');
+                var author = object.get('userName');
+                var agree = object.get('numberOfAgree');
+                var disagree = object.get('numberOfDisagree');
+                var inHtml_title = '<p class=stat-mostDisagree>Most Controversial <br></p>'; 
+                var inHtml_text = '<p class=stat-text>'+'"' + selectedText + '"</p>';
+                var inHtml_author = '<p class=stat-author>' +'--by '+ author + '</p>';
+                var inHtml_agree = '<span class=stat-agree id=con_agree>' + agree + '</span>';
+                var inHtml_disagree = '<span class=stat-agree id=con_disagree>' + disagree + '</span>';
+                var inHtml_con = inHtml_title + inHtml_text + inHtml_author + btnup +inHtml_agree +  btndown + inHtml_disagree;
+                inHtml= inHtml_pop + inHtml_con;
+                $("#tweet-stat").html(inHtml);
+                bindEvent();
+              });
+            },
+            error: function(error) {
+              alert("Error: " + error.code + " " + error.message);
+            }
+          });
       });
     },
     error: function(error) {
       alert("Error: " + error.code + " " + error.message);
     }
+  });
+
+}
+
+function queryCurrentUserPop(annotationId, userId, _callback){
+  var UserAnnotation = Parse.Object.extend("UserAnnotation");
+  var query = new Parse.Query(UserAnnotation);
+  query.equalTo('annotationId', annotationId);
+  query.equalTo('userId', userId);
+  query.find({
+      success: function(results) {
+        _callback(results);
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+  });
+}
+
+function queryCurrentUserCon(annotationId, userId, _callback){
+  var UserAnnotation = Parse.Object.extend("UserAnnotation");
+  var query = new Parse.Query(UserAnnotation);
+  query.equalTo('annotationId', annotationId);
+  query.equalTo('userId', userId);
+  query.find({
+      success: function(results) {
+        _callback(results);
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+  });
+}
+  
+
+function bindEvent(){
+  $("#thumbup").click(function(){
+    console.log('clicked up');
+  });
+  
+  $("#thumbdown").click(function(){
+    console.log('clicked down');
   });
 }
 
@@ -69,6 +128,7 @@ function showNickname(){
   var inHtml2 = currentUser.get("nickname") + ' !' + '</h1>';
   var inHtml = inHtml1 + inHtml2;
   $("#welcome-toggle").html(inHtml);
+  return currentUser.id;
 }
 
 
