b0765f301b9b6d744fa50ec410708bd39c914f19
chijuzipi <yuzhoubrother@gmail.com>
2014-09-17 03:53:24 -0500

    whole word selection bug fix

---
+++ js/processor/modules/processor.disqus.js
@@ -0,0 +1,40 @@
+/*
+  processor.twitter.js
+
+  (c) 2014 by archlyx
+*/
+
+;(function(processor, $) {
+  processor.addModule({
+    disqus: {
+      initElements: "#post-list",
+      container: ".post-body",
+
+      getInfoFromContainer: function(element) {
+        var post-byline = $(element).find('.post-byline');
+        var post-meta = $(element).find('.post-meta');
+        
+        var userName   = $(post-byline).find("a[data-role=username]").val();
+        var commentURL = $(post-meta).find("a").attr("href");
+        //var commentID = 
+        //console.log("userName is : ", userName, "  commentURL is: ", commentURL);
+
+        return {postId: commentURL, userName: userName};
+      },
+
+      initializeUpdateEvent: function() {
+      console.log("init update event");
+      /*
+        $(window).scroll(function() {
+          var origTweetNumber = Object.keys(processor.postList).length;
+          var newTweetNumber = $(processor.container).length;
+
+          if (newTweetNumber > origTweetNumber) {
+            $(window).trigger("postUpdated");
+          }
+        });
+        */
+      }
+    }
+  });
+})(processor, jQuery);
