9c9facc9c5bfd13542564dc1652f2f0fd78a05b0
chijuzipi <yuzhoubrother@gmail.com>
2014-09-22 13:41:14 -0500

    show top 10 annotations

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -10,6 +10,7 @@ var _conObject;
 $(document).ready(function(){
     var currentUserId = showNickname();
     //generateToggleHTML(currentUserId);
+      
     generateToggleHTML(currentUserId, function(){
       bindEvent(currentUserId);
     });
@@ -18,21 +19,96 @@ $(document).ready(function(){
 function generateToggleHTML(currentUserId, _callback) {
 
   var Annotation = Parse.Object.extend("Annotation");
-  var UserAnnotation = Parse.Object.extend("UserAnnotation");
   var query = new Parse.Query(Annotation);
   query.descending("numberOfAgree");
   query.limit(10);
   query.find({
     success: function(objects) {
+      var inHtml_title = '<p class=stat-title id=stat-title>top 10 popular annotations<br></p><hr>'; 
+      $("#post-stat-pop").html(inHtml_title);
       //queryCurrentUser(objects, currentUserId);
       for (var i = 0; i < objects.length; i++){
-        generateAnnotation(objects[i]);
+        generateAnnotation(objects[i], i);
       }
+      query.descending("numberOfDisagree");
+      query.limit(10);
+      query.find({
+        success: function(objects) {
+          var inHtml_title = '<p class=stat-title id=stat-title>top 10 controversial annotations<br></p><hr>'; 
+          $("#post-stat-pop").append(inHtml_title);
+          //queryCurrentUser(objects, currentUserId);
+          for (var i = 0; i < objects.length; i++){
+            generateAnnotation(objects[i], i+10);
+          }
+          _callback();
+        }
+      });
     }
   });
-  _callback();
 }
 
+function generateAnnotation(object, index){
+  //var opinion;
+    var btnup_pop = makeButton ('btnup_pop', 'gray');
+    var btndown_pop = makeButton ('btndown_pop', 'gray');
+    var selectedText = object.get('selectedText');
+    var author = object.get('userName');
+    var agree = object.get('numberOfAgree');
+    var disagree = object.get('numberOfDisagree');
+    var source = object.get('hostDomain');
+    var inHtml_source = '<p class=stat-source>from ' + source + ': </p>'; 
+    var inHtml_text = '<p class=stat-text id=stat-text-'+ index +'> " ' + selectedText + ' "</p>';
+    var inHtml_author = '<p class=stat-author>' +'--by '+ author + '</p>';
+    var inHtml_agree = '<span class=stat-agree id=pop_agree>' + agree + '</span>';
+    var inHtml_disagree = '<span class=stat-disagree id=pop_disagree>' + disagree + '</span>';
+    var inHtml_goPost = '<span class=stat-goPost id=pop_goPost_'+ index+'> see original post </span>';
+    var inHtml_pop = inHtml_source + inHtml_text + inHtml_author + btnup_pop + inHtml_agree + btndown_pop + inHtml_disagree + inHtml_goPost + '<hr>';
+    $("#post-stat-pop").append(inHtml_pop);
+    var linkId = '#pop_goPost_' + index;
+    $(linkId).data("annotation", object);
+}
+
+function generateNewTab(node){
+  //console.log(node.data());
+  var postId = node.data("annotation").get('postId');
+  var userName = node.data("annotation").get('userName');
+  url = 'https://twitter.com/' + userName + '/status/' + postId;
+  window.open(url);
+}
+
+function bindEvent(userId){
+
+/*
+  $('#thumbup_pop, #thumbdown_pop, #thumbup_con, #thumbdown_con').click(function(){
+    processVote($(this), userId);
+  });
+  
+  $('#thumbup_pop, #thumbdown_pop, #thumbup_con, #thumbdown_con').click(function(){
+    generateModal($(this));
+  });
+*/
+  for (var i = 0; i < 20; i++){
+    var linkId = '#pop_goPost_' + i;
+    $(linkId).click(function(){
+      generateNewTab($(this));
+    });
+  }
+ 
+  $("#welcome-logout").click(function(){
+      Parse.User.logOut();
+      removeStorage();
+      chrome.browserAction.setIcon({path:'../../img/T-400_white.png'}, function()
+      {
+        window.close();
+      });
+  });
+  
+  $("#welcome-close").click(function(){
+    window.close();
+  });
+}
+
+/*
 function queryCurrentUser(objects, userId, _callback){
   var UserAnnotation = Parse.Object.extend("UserAnnotation");
   var query = new Parse.Query(UserAnnotation);
@@ -47,103 +123,7 @@ function queryCurrentUser(objects, userId, _callback){
       }
   });
 }
-
-function generateAnnotation(object, currentUserId){
-  //var opinion;
-  var btnup_pop = makeButton ('btnup_pop', 'gray');
-  var btndown_pop = makeButton ('btndown_pop', 'gray');
-  var btnup_con = makeButton ('btnup_con', 'gray');
-  var btndown_con = makeButton ('btndown_con', 'gray');
-    var selectedText = object.get('selectedText');
-    var author = object.get('userName');
-    var agree = object.get('numberOfAgree');
-    var disagree = object.get('numberOfDisagree');
-    var source = object.get('hostDomain');
-    var inHtml_title = '<p class=stat-mostAgree>Most popular (' + source + '): <br></p>'; 
-    var inHtml_text = '<p class=stat-text>'+'" ' + selectedText + ' "</p>';
-    var inHtml_author = '<p class=stat-author>' +'--by '+ author + '</p>';
-    var inHtml_agree = '<span class=stat-agree id=pop_agree>' + agree + '</span>';
-    var inHtml_disagree = '<span class=stat-disagree id=pop_disagree>' + disagree + '</span>';
-    var inHtml_goPost = '<span class=stat-disagree id=pop_goPost> see original post </span>';
-    var inHtml_pop = inHtml_title + inHtml_text + inHtml_author + btnup_pop + inHtml_agree + btndown_pop + inHtml_disagree + inHtml_goPost;
-    $("#post-stat-pop").append(inHtml_pop);
-}
-  
-    /*
-      _popUserName = object.get('userName'); 
-      _popPostId = object.get('postId');
-      _popObject = object;
-      queryCurrentUser(object.id, currentUserId, function(result){
-          var opinion;
-          if (result.length === 0){opinion = 0;}
-          else {opinion = result[0].get('opinion');}
-          var selectedText = object.get('selectedText');
-          var author = object.get('userName');
-          var agree = object.get('numberOfAgree');
-          var disagree = object.get('numberOfDisagree');
-          var source = object.get('hostDomain');
-          var inHtml_title = '<p class=stat-mostAgree>Most popular (' + source + '): <br></p>'; 
-          var inHtml_text = '<p class=stat-text>'+'" ' + selectedText + ' "</p>';
-          var inHtml_author = '<p class=stat-author>' +'--by '+ author + '</p>';
-          var inHtml_agree = '<span class=stat-agree id=pop_agree>' + agree + '</span>';
-          var inHtml_disagree = '<span class=stat-disagree id=pop_disagree>' + disagree + '</span>';
-          var inHtml_goPost = '<span class=stat-disagree id=pop_goPost> see original post </span>';
-
-          if(opinion === 1)
-            btnup_pop = makeButton('btnup_pop', 'blue');
-          else if(opinion === -1)
-            btndown_pop = makeButton('btndown_pop', 'blue');
-
-          var inHtml_pop = inHtml_title + inHtml_text + inHtml_author + btnup_pop + inHtml_agree + btndown_pop + inHtml_disagree + inHtml_goPost;
-          $("#post-stat-pop").html(inHtml_pop);
-          //$(".stat-mostAgree").data("object", object);
-          query.descending("numberOfDisagree");
-          query.first({
-            success: function(object) {
-              //chrome.storage.sync.set({conAnnotation: object});
-              _conUserName = object.get('userName'); 
-              _conPostId = object.get('postId');
-              _conObject = object;
-              queryCurrentUser(object.id, currentUserId, function(result){
-                var opinion;
-                if (result.length === 0){opinion = 0;}
-                else {opinion = result[0].get('opinion');}
-                var selectedText = object.get('selectedText');
-                var author = object.get('userName');
-                var agree = object.get('numberOfAgree');
-                var disagree = object.get('numberOfDisagree');
-                var source = object.get('hostDomain');
-                var inHtml_title = '<p class=stat-mostDisagree>Most controversial (' + source + '): <br></p>'; 
-                var inHtml_text = '<p class=stat-text>'+'" ' + selectedText + ' "</p>';
-                var inHtml_author = '<p class=stat-author>' +'--by '+ author + '</p>';
-                var inHtml_agree = '<span class=stat-agree id=con_agree>' + agree + '</span>';
-                var inHtml_disagree = '<span class=stat-disagree id=con_disagree>' + disagree + '</span>';
-                var inHtml_goPost = '<span class=stat-disagree id=con_goPost> see original post </span>';
-
-                if(opinion === 1)
-                  btnup_con = makeButton('btnup_con', 'blue');
-                else if(opinion === -1)
-                  btndown_con = makeButton('btndown_con', 'blue');
-
-                var inHtml_con = inHtml_title + inHtml_text + inHtml_author + btnup_con +inHtml_agree +  btndown_con + inHtml_disagree + inHtml_goPost;
-                $("#post-stat-con").html(inHtml_con);
-                //$(".stat-mostDisagree").data("object", object);
-
-                _callback();
-              });
-            },
-            error: function(error) {
-              alert("Error: " + error.code + " " + error.message);
-            }
-          });
-      });
-    },
-    error: function(error) {
-      alert("Error: " + error.code + " " + error.message);
-    }
-  });
-}
-  */
+*/
 
 function makeButton(btn, color){
   var btnClass;
@@ -170,37 +150,8 @@ function makeButton(btn, color){
   return button;
 }
 
-  //var btnup_pop = '<span class="btnup" id=thumbup_pop style="color:gray;" title="" data-toggle="modal" data-target="#myModal"><i class="fa fa-thumbs-up"></i></span>';
 
-  
-function bindEvent(userId){
-
-  $('#thumbup_pop, #thumbdown_pop, #thumbup_con, #thumbdown_con').click(function(){
-    processVote($(this), userId);
-  });
-  
-  $('#thumbup_pop, #thumbdown_pop, #thumbup_con, #thumbdown_con').click(function(){
-    generateModal($(this));
-  });
-  
-  $('#pop_goPost, #con_goPost').click(function(){
-    generateNewTab($(this));
-  });
- 
-  $("#welcome-logout").click(function(){
-      Parse.User.logOut();
-      removeStorage();
-      chrome.browserAction.setIcon({path:'../../img/T-400_white.png'}, function()
-      {
-        window.close();
-      });
-  });
-  
-  $("#welcome-close").click(function(){
-    window.close();
-  });
-}
-
+/*
 function generateModal(node){
   if (node.attr('id') === 'thumbup_pop'){
     var wholePost = _popObject.get('wholePost');
@@ -229,21 +180,6 @@ function generateModal(node){
       cssApplier.applyToRange(range);
     } else return;
 }
-  
-
-function generateNewTab(node){
-  console.log('clicked');
-  if(node.attr('id') === 'pop_goPost'){
-    _popUserName;
-    url = 'https://twitter.com/' + _popUserName + '/status/' + _popPostId;
-    window.open(url);
-  }
-  else {
-    var userName = _conUserName;
-    url = 'https://twitter.com/' + _conUserName + '/status/' + _conPostId;
-    window.open(url);
-   } 
-}
 
 function processVote(node,userId){
   var num;
@@ -261,12 +197,10 @@ function processVote(node,userId){
     counterNumNode = $('#pop_disagree');
     counterNum = parseInt(counterNumNode.html());
     annotationObject = $("stat-mostAgree").data("object");
-    /*
     if(node.css("color") === "rgb(0, 0, 255)") 
       chrome.storage.sync.set({thumbup_pop: 0});
     else
       chrome.storage.sync.set({thumbup_pop: 1});
-    */
   }
 
   else if (node.attr('id') === 'thumbdown_pop'){
@@ -276,12 +210,10 @@ function processVote(node,userId){
     counterNumNode = $('#pop_agree');
     counterNum = parseInt(counterNumNode.html());
     annotationObject = $("stat-mostAgree").data("object");
-    /*
     if(node.css("color") === "rgb(0, 0, 255)") 
       chrome.storage.sync.set({thumbdown_pop: 0});
     else
       chrome.storage.sync.set({thumbdown_pop: 1});
-    */
   }
   
   else if (node.attr('id') === 'thumbup_con'){
@@ -322,6 +254,7 @@ function processVote(node,userId){
     //chrome.storage.sync.set({popAnnotation: object});
     //counterBtn.css({"color":""});
 }
+*/
 
 function removeStorage(){
   chrome.storage.sync.set({objectId: "", username: "", nickname:""}, function(){
