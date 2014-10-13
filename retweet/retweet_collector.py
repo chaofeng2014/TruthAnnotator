71c0690072292fe41ee3263377a79d9d37788acb
Fang Wang <fangwang@Yus-MacBook-Pro.local>
2014-10-13 10:10:04 -0500

    retweet collecotr added

---
+++ retweet/retweet_collector.py
@@ -0,0 +1,20 @@
+import tweepy
+
+consumer_key = "Aey8cEiIJcp02HjbUl74M2JrV"
+consumer_secret = "bHDf717W4IQgOx42g4g5uCT8ffZwqrtaNJBDOiSl4qrqgFXquq"
+access_token = "2600799996-TUJf2enCDoUSVCwAl9ahBFs1Cuit01XgACn9e70"
+access_token_secret = "MJWzdkgYkPBvrhIVNrf0zTSqSefh1gqa9XmY9dqTcEWSd"
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+tweetId = "521660698162905088"
+recentRetweets = api.retweets(tweetId,3)
+
+for tweet in recentRetweets:
+  print tweet.text
+  print
+  print tweet.author
